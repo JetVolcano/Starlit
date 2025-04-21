6be2f2752c10568b401a857a63ab64c138de6475
JetVolcano <184006209+JetVolcano@users.noreply.github.com>
2025-04-20 20:21:02 -0700

    Add eventbus to register items

--- src/main/java/com/jetvolcano/starlit_beings/item/ModItems.java
+++ src/main/java/com/jetvolcano/starlit_beings/item/ModItems.java
@@ -1,8 +1,12 @@
 package com.jetvolcano.starlit_beings.item;
 
 import com.jetvolcano.starlit_beings.StarlitBeings;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(StarlitBeings.MOD_ID);
+    public static void register(IEventBus eventbus) {
+        ITEMS.register(eventbus);
+    }
 }

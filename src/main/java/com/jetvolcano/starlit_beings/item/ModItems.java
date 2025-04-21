947c0c709a37e506deb4312e599ee35f7a81d900
JetVolcano <184006209+JetVolcano@users.noreply.github.com>
2025-04-20 20:08:22 -0700

    Update mod configs

---
+++ src/main/java/com/jetvolcano/starlit_beings/item/ModItems.java
@@ -0,0 +1,8 @@
+package com.jetvolcano.starlit_beings.item;
+
+import com.jetvolcano.starlit_beings.StarlitBeings;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(StarlitBeings.MOD_ID);
+}

382f7a47628da1eccb9bf0d387ea94adfd8b4c3a
JetVolcano <184006209+JetVolcano@users.noreply.github.com>
2025-04-21 16:36:44 -0700

    Rename parameter ModItems.java.ModItems.register eventbus to eventBus
    Signed-off-by: JetVolcano <184006209+JetVolcano@users.noreply.github.com>

--- src/main/java/com/jetvolcano/starlit_beings/item/ModItems.java
+++ src/main/java/com/jetvolcano/starlit_beings/item/ModItems.java
@@ -6,7 +6,7 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(StarlitBeings.MOD_ID);
-    public static void register(IEventBus eventbus) {
-        ITEMS.register(eventbus);
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
     }
 }

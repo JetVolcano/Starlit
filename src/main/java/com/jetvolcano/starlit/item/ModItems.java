1393ca547b0e92d8c145a628b7b72e946e45ca8b
JetVolcano <184006209+JetVolcano@users.noreply.github.com>
2025-04-22 16:57:57 -0700

    Fix import in ModItems.java

--- src/main/java/com/jetvolcano/starlit/item/ModItems.java
+++ src/main/java/com/jetvolcano/starlit/item/ModItems.java
@@ -1,13 +1,13 @@
 package com.jetvolcano.starlit.item;
 
-import com.jetvolcano.starlit.StarlitBeings;
+import com.jetvolcano.starlit.Starlit;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModItems {
-    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(StarlitBeings.MOD_ID);
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Starlit.MOD_ID);
     public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () -> new Item(new Item.Properties()));
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
